400d75b25b33e8da99082c324bb769f8a5397f85
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-07-24 21:21:39 -0400

    robofriends scaffolding

---
+++ src/app/projects/(react-simple)/robofriends/page.jsx
@@ -0,0 +1,13 @@
+import Layout from '../../../components/Layout.component';
+
+const RobofriendsPage = () => {
+  return (
+    <Layout>
+      <main className="flex flex-col items-center">
+        <h1 className="uppercase">robofriends</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default RobofriendsPage;
