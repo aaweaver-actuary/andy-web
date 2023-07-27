1e0c6b4548fdc7a1d2c22e6881f64d59e3126988
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-07-26 20:31:07 -0400

    build facedetection page itself

---
+++ src/app/projects/(react-complex)/face-detection/page.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../../components/layout';
+
+const FaceDetectionPage = () => {
+  return (
+    <Layout>
+      <h1>Face detection page</h1>
+    </Layout>
+  );
+};
+
+export default FaceDetectionPage;
