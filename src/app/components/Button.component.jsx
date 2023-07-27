a01d2b50ad384320717bf89a069984cded31366d
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-07-26 21:39:10 -0400

    what really happens if i do this

--- src/app/components/Button.component.jsx
+++ src/app/components/Button.component.jsx
@@ -1,14 +1,13 @@
-import { Button as MButton } from '@mui/material';
+// import { Button as MButton } from '@mui/material';
 
 const Button = ({ text, onClick = () => null }) => {
   return (
-    <MButton
-      variant="contained"
+    <button
       className={`lowercase text-md ubuntu z-[1000] rounded-xl text-black border-black border-[2px] mx-2 hover:bg-black hover:text-white`}
       onClick={onClick}
     >
       {text}
-    </MButton>
+    </button>
   );
 };
 
