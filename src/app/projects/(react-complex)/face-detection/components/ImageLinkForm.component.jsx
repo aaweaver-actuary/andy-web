0f2679e8e4ddf851795e48a17a219a72daeefc1b
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-07-27 01:03:37 -0400

    scaffolded main parts of image link form

--- src/app/projects/(react-complex)/face-detection/components/ImageLinkForm.component.jsx
+++ src/app/projects/(react-complex)/face-detection/components/ImageLinkForm.component.jsx
@@ -1,12 +1,31 @@
+'use client';
+
+import Button from '../../../../components/Button.component';
+
 const ImageLinkForm = () => {
   return (
     <div
       className={`
-    border-blue-500 border-2 bg-white bg-opacity-30
-      my-3 p-3
-      `}
+      border-blue-500 border-2 bg-white bg-opacity-30
+        my-3 p-3
+        `}
     >
-      <h1>IMAGE LINK FORM</h1>
+      <p className="text-sm">
+        {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+      </p>
+      <div className="mt-2">
+        <input
+          type="text"
+          className="text-md p-2 w-[70vw] text-center border-gray-300 border-2 rounded-lg"
+        />
+        <Button
+          className="text-md p-2 w-[30vw] text-center"
+          text="Detect"
+          onClick={() => {
+            console.log('detected detect!');
+          }}
+        />
+      </div>
     </div>
   );
 };
