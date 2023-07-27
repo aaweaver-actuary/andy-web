a337a3c027dedd6309a3e10d49b50a813fbb7fa9
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-07-27 03:34:56 -0400

    sdfkaj

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -15,25 +15,27 @@ const ProjectsPage = () => {
 
   return (
     <Layout>
-      <h2 className="text-3xl top-1 ubuntu px-7">.projects</h2>
+      <main className="h-full w-[90%] flex flex-col items-center">
+        <h2 className="text-3xl top-1 ubuntu px-7">.projects</h2>
 
-      <div className="w-[vw100] grid grid-flow-row lg:grid-cols-3 mt-3">
-        <ProjectAccordion
-          title={'.react-projects'}
-          dataType={['.simple-react', '.complex-react']}
-          data={projectsData['.react-projects']}
-        />
-        <ProjectAccordion
-          title={'.learning-projects'}
-          dataType={['.promises']}
-          data={projectsData['.learning-projects']}
-        />
-        <ProjectAccordion
-          title={'.typescript-projects'}
-          dataType={['.simple-ts', '.complex-ts']}
-          data={projectsData['.typescript-projects']}
-        />
-      </div>
+        <div className="w-[vw100] grid grid-flow-row lg:grid-cols-3 mt-3">
+          <ProjectAccordion
+            title={'.react-projects'}
+            dataType={['.simple-react', '.complex-react']}
+            data={projectsData['.react-projects']}
+          />
+          <ProjectAccordion
+            title={'.learning-projects'}
+            dataType={['.promises']}
+            data={projectsData['.learning-projects']}
+          />
+          <ProjectAccordion
+            title={'.typescript-projects'}
+            dataType={['.simple-ts', '.complex-ts']}
+            data={projectsData['.typescript-projects']}
+          />
+        </div>
+      </main>
     </Layout>
   );
 };
